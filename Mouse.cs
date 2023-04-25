7fbc6d784d39e80d77dd50efe7cf653336d5e0d7
BanditTech <thebbandit@gmail.com>
2023-04-25 16:14:22 -0700

    add a mouse click class

---
+++ Mouse.cs
@@ -0,0 +1,73 @@
+﻿namespace Triggered
+{
+    using System;
+    using System.Globalization;
+    using System.Runtime.InteropServices;
+    public class Mouse
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern void mouse_event(uint dwFlags, uint dx, uint dy, int dwData, UIntPtr dwExtraInfo);
+        private const uint MOUSEEVENTF_LEFTDOWN = 0x0002;
+        private const uint MOUSEEVENTF_LEFTUP = 0x0004;
+        private const uint MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        private const uint MOUSEEVENTF_RIGHTUP = 0x0010;
+        private const uint MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+        private const uint MOUSEEVENTF_MIDDLEUP = 0x0040;
+        private const uint MOUSEEVENTF_XDOWN = 0x0080;
+        private const uint MOUSEEVENTF_XUP = 0x0100;
+        private const uint MOUSEEVENTF_WHEEL = 0x0800;
+        private const uint MOUSEEVENTF_HWHEEL = 0x1000;
+        private static TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+
+        public static void Click(string methodname, int x, int y)
+        {
+            string MethodName = textInfo.ToTitleCase(methodname);
+            var mouse = new Mouse();
+            var method = typeof(Mouse).GetMethod(MethodName);
+            if (method != null)
+            {
+                method.Invoke(mouse, new object[] { x, y });
+            }
+            else
+            {
+                throw new ArgumentException($"Invalid click method name: {methodname}");
+            }
+        }
+        public static void Left(int x, int y)
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)x, (uint)y, 0, UIntPtr.Zero);
+        }
+        public static void Right(int x, int y)
+        {
+            mouse_event(MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, (uint)x, (uint)y, 0, UIntPtr.Zero);
+        }
+        public static void Middle(int x, int y)
+        {
+            mouse_event(MOUSEEVENTF_MIDDLEDOWN | MOUSEEVENTF_MIDDLEUP, (uint)x, (uint)y, 0, UIntPtr.Zero);
+        }
+        public static void M4(int x, int y)
+        {
+            mouse_event(MOUSEEVENTF_XDOWN | MOUSEEVENTF_XUP, (uint)x, (uint)y, 1, UIntPtr.Zero);
+        }
+        public static void M5(int x, int y)
+        {
+            mouse_event(MOUSEEVENTF_XDOWN | MOUSEEVENTF_XUP, (uint)x, (uint)y, 2, UIntPtr.Zero);
+        }
+        public static void WheelUp(int x, int y)
+        {
+            mouse_event(MOUSEEVENTF_WHEEL, (uint)x, (uint)y, 120, UIntPtr.Zero);
+        }
+        public static void WheelDown(int x, int y)
+        {
+            mouse_event(MOUSEEVENTF_WHEEL, (uint)x, (uint)y, -120, UIntPtr.Zero);
+        }
+        public static void WheelLeft(int x, int y)
+        {
+            mouse_event(MOUSEEVENTF_HWHEEL, (uint)x, (uint)y, -120, UIntPtr.Zero);
+        }
+        public static void WheelRight(int x, int y)
+        {
+            mouse_event(MOUSEEVENTF_HWHEEL, (uint)x, (uint)y, 120, UIntPtr.Zero);
+        }
+    }
+}
