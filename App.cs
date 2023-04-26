46d5fceecfc0b23b1819f40c5e843144490a9bea
BanditTech <thebbandit@gmail.com>
2023-04-26 02:28:23 -0700

    allow App.Log call from anywhere in the app

--- App.cs
+++ App.cs
@@ -2,36 +2,35 @@
 {
     using NLog;
     using NLog.Config;
+    using System.Collections.Generic;
     using System.Diagnostics;
-    internal class App
+    public static class App
     {
-        public readonly Stopwatch Watch = Stopwatch.StartNew();
-        public bool MenuDisplay_Main = true;
-        public bool MenuDisplay_Log = true;
-        public bool IsRunning = true;
-        public int LogicTickDelayInMilliseconds = 100;
-        public ExampleAppLog log;
-        public Logger logger;
+        public static Dictionary<string, object> data;
+        public static ExampleAppLog logimgui;
+        public static Logger logger;
+        public static int selectedLogLevelIndex;
 
-        public App()
+        static App()
         {
             LogManager.Configuration = new XmlLoggingConfiguration("nlog.config");
-            log = new ExampleAppLog();
+            logimgui = new ExampleAppLog();
             logger = LogManager.GetCurrentClassLogger();
+            data = I.O.Data;
         }
         #region Log(string log, LogLevel level)
-        public void Log(string log, LogLevel level)
+        public static void Log(string log, LogLevel level)
         {
             // Only send message to the log window above Debug level
-            if (level.Ordinal >= LogLevel.Debug.Ordinal)
+            if (level.Ordinal >= ((LogLevel)data["LogWindowMinimumLogLevel"]).Ordinal)
             {
-                this.log.AddLog(string.Format("{0}: {1}", level.ToString(), log),level);
+                logimgui.AddLog(string.Format("{0}: {1}", level.ToString(), log),level);
             }
-            this.logger.Log(level,log);
+            logger.Log(level,log);
         }
-        public void Log(string log)
+        public static void Log(string log)
         {
-            this.Log(log, LogLevel.Info);
+            Log(log, LogLevel.Info);
         }
         #endregion
     }
