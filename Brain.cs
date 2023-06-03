dc95f3e1c8398e2d7ba2494d37c6e029535e7514
BanditTech <thebbandit@gmail.com>
2023-06-02 23:52:47 -0700

    introduce Brain

---
+++ Brain.cs
@@ -0,0 +1,24 @@
+﻿namespace Triggered
+{
+    public static class Brain
+    {
+        private static float Health => App.Player.Health;
+        private static float Mana => App.Player.Mana;
+        private static float EnergyShield => App.Player.EnergyShield;
+        private static string Location => App.Player.Location;
+
+
+        static Brain()
+        {
+            // Give some thought
+        }
+
+        /// <summary>
+        /// Logical processing.
+        /// </summary>
+        public static void Process()
+        {
+            // Ponder our logic
+        }
+    }
+}
