ec2bc8cb13c756bc6c034792c5b6ee3f8c2a2c5c
BanditTech <thebbandit@gmail.com>
2023-06-05 13:32:21 -0700

    locations panel

--- modules/panel/Locations.cs
+++ modules/panel/Locations.cs
@@ -1,4 +1,6 @@
 ﻿using Triggered.modules.options;
+using static Triggered.modules.wrapper.PointScaler;
+using ImGuiNET;
 
 namespace Triggered.modules.panel
 {
@@ -7,9 +9,24 @@ namespace Triggered.modules.panel
         private static Options_Locations Opts => App.Options.Locations;
         internal static void Render()
         {
-            if (Opts == null)
+            foreach (var location in Opts.Iterate())
             {
-                // We got a problem cowboy.
+                switch (location)
+                {
+                    case Coordinate coordinate:
+                        ImGui.Button("Here");
+                        // Code for handling Coordinate type
+                        break;
+                    case ScaledRectangle scaledRectangle:
+                        // Code for handling ScaledRectangle type
+                        break;
+                    case Measurement measurement:
+                        // Code for handling Measurement type
+                        break;
+                    default:
+                        // Default case if the type doesn't match any of the expected types
+                        break;
+                }
             }
         }
     }
