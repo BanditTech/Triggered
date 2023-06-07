b9ecac0fc41b6c221406d2cd4191fa104be2cadf
BanditTech <thebbandit@gmail.com>
2023-06-06 21:03:30 -0700

    add some style to the created menu

--- modules/panel/Locations.cs
+++ modules/panel/Locations.cs
@@ -3,6 +3,7 @@ using static Triggered.modules.wrapper.PointScaler;
 using ImGuiNET;
 using System.Diagnostics.CodeAnalysis;
 using Triggered.modules.wrapper;
+using System;
 
 namespace Triggered.modules.panel
 {
@@ -11,6 +12,9 @@ namespace Triggered.modules.panel
         private static string _selected;
         private static Options_Locations Opts => App.Options.Locations;
         private static Options_Panel Panel => App.Options.Panel;
+        private static readonly Type anchorPosType = typeof(AnchorPosition);
+        private static readonly string[] anchorNames = Enum.GetNames(anchorPosType);
+        private static readonly Array anchorValues = Enum.GetValues(anchorPosType);
 
         [RequiresDynamicCode("Calls Triggered.modules.options.Options.IterateObjects()")]
         internal static void Render()
@@ -22,8 +26,27 @@ namespace Triggered.modules.panel
             {
                 if (obj is ScaledRectangle scaledRectangle)
                 {
-                    if (ImGui.Button(key))
+                    ImGui.Text($"Scaled Rectangle: {key}");
+                    ImGui.PushID($"{key} Button");
+                    ImGui.Indent(40);
+                    if (ImGui.Button("Select Area"))
                         _selected = key;
+                    ImGui.PopID();
+                    ImGui.SameLine();
+                    ImGui.Text("Anchor:");
+                    ImGui.SameLine();
+                    ImGui.PushID($"{key} Combo");
+                    ImGui.SetNextItemWidth(ImGui.GetContentRegionAvail().X);
+                    var anchorIndex = Array.IndexOf(anchorValues, scaledRectangle.Start.Anchor);
+                    if (ImGui.Combo("##Anchor{key}",ref anchorIndex, anchorNames, anchorNames.Length))
+                    {
+                        string anchorPositionName = Enum.GetName(anchorPosType, anchorIndex);
+                        AnchorPosition newAnchorPosition = (AnchorPosition)Enum.Parse(anchorPosType, anchorPositionName);
+                        scaledRectangle.Start = new(scaledRectangle.Start.Point, scaledRectangle.Start.Height, newAnchorPosition);
+                        scaledRectangle.End = new(scaledRectangle.End.Point, scaledRectangle.End.Height, newAnchorPosition);
+                        Opts.SetKey(key,scaledRectangle);
+                    }
+                    ImGui.PopID();
                     if (_selected == key)
                     {
                         if (Selector.ScaledRectangle(ref scaledRectangle,scaledRectangle.Start.Anchor))
@@ -32,6 +55,8 @@ namespace Triggered.modules.panel
                             _selected = null;
                         }
                     }
+                    ImGui.Spacing();
+                    ImGui.Unindent(40);
                 }
                 else if (obj is Coordinate coordinate)
                 {
