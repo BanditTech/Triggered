db702d32c11655c0b5a76214b0d8bfb2f1a630f0
BanditTech <thebbandit@gmail.com>
2023-06-09 10:04:56 -0700

    add callback class

---
+++ modules/wrapper/Callbacks.cs
@@ -0,0 +1,10 @@
+﻿namespace Triggered.modules.wrapper
+{
+    internal static class Callbacks
+    {
+        internal static void FontIndexEdit(int index)
+        {
+            App.Options.Font.SetKey("Name", App.fonts[index]);
+        }
+    }
+}
