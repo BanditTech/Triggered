c33b0bcb29035e3ef63aeea8e16a2dfa6673b8ad
BanditTech <thebbandit@gmail.com>
2023-06-09 22:38:44 -0700

    begin windows wrapper file

---
+++ modules/wrapper/Windows.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Triggered.modules.wrapper
+{
+    public static class Windows
+    {
+        public static IntPtr GetProcessHandle(string exeFileName = "PathOfExile")
+        {
+            Process[] processes = Process.GetProcessesByName(exeFileName);
+            IntPtr hwnd = IntPtr.Zero;
+            if (processes.Length > 0)
+                hwnd = processes[0].MainWindowHandle;
+            return hwnd;
+        }
+    }
+}
