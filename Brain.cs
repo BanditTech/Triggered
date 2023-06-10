9a6c6c824fb1b948bcd6e495b23ab5870a071a28
BanditTech <thebbandit@gmail.com>
2023-06-09 22:48:47 -0700

    poetry in motion

--- Brain.cs
+++ Brain.cs
@@ -12,9 +12,21 @@ namespace Triggered.modules.wrapper
     {
         private static float Health => App.Player.Health;
         private static float Mana => App.Player.Mana;
-        private static float EnergyShield => App.Player.EnergyShield;
+        private static float Shield => App.Player.Shield;
+        private static float Ward => App.Player.Ward;
+        private static float Rage => App.Player.Rage;
         private static string Location => App.Player.Location;
+
+        /// <summary>
+        /// In the realm of machine vision, algorithms unveil hidden truths,
+        /// Perceiving details through unblinking eyes, where beauty and knowledge fuse.
+        /// With digital sight, they paint visual tales,
+        /// Where innovation thrives and wisdom prevails.
+        /// In this realm of code and light, a symphony unfolds,
+        /// Pixels dance, revealing insights untold.
+        /// </summary>
         public static Mat Vision { get; set; }
+
         /// <summary>
         /// Logical processing.
         /// </summary>
